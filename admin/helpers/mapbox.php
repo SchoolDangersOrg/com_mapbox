24b2b6893a5a1113a8555f9e333ec08b110449bf
Alonzo Turner <alonzo.turner@subtextproductions.com>
2014-04-08 11:35:05 -0500

    FIXED: Missing comma

--- admin/helpers/mapbox.php
+++ admin/helpers/mapbox.php
@@ -14,6 +14,6 @@ abstract Class MapboxHelper {
 	public static function addSubmenu($submenu){
 		// ADD SUBMENU TABS
 		JSubMenuHelper::addEntry(JText::_('COM_MAPBOX_SUBMENU_MAPS'), 'index.php?option=com_mapbox', $submenu == 'mapbox');
-		JSubMenuHelper::addEntry(JText::_('COM_MAPBOX_SUBMENU_MARKERS'), 'index.php?option=com_mapbox&view=markers' $submenu == 'markers');
+		JSubMenuHelper::addEntry(JText::_('COM_MAPBOX_SUBMENU_MARKERS'), 'index.php?option=com_mapbox&view=markers', $submenu == 'markers');
 	}
 }
